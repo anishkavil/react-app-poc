dbc25200af17324c1eea29fb593672405ca5dd04
Anish PK <anish.kavil@gmail.com>
2024-07-05 17:11:41 +0530

    initial commit

--- README.md
+++ README.md
@@ -68,3 +68,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+docker run -p 3000:3000 -v $(pwd):/app ef63c978af2c4aa519c
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('I was changed for good ', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I was changed for good/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('I was changed for good ', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/I was changed for good/i);
   expect(linkElement).toBeInTheDocument();
 });
